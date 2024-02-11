e8cd04f8e03e2ad41c40966c62f3a5f3eaf54213
Elijah Jackson <135048236+Redtrooper@users.noreply.github.com>
2024-02-11 13:54:37 -0500

    PauseMenu & GameManager Setup
    Setup the game manager script and implemented a pause menu.

---
+++ ProjectAndPortfolio2TeamProject/Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunctions : MonoBehaviour
+{
+    public void resume()
+    {
+        gameManager.instance.stateUnpaused();
+        gameManager.instance.lockCursor();
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameManager.instance.stateUnpaused();
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ ProjectAndPortfolio2TeamProject/Assets/Scripts/gameManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+    private float timeScale;
+
+    private GameObject activeMenu;
+    [SerializeField] GameObject pauseMenu;
+
+    private bool isPaused;
+    void Awake()
+    {
+        instance = this;
+        timeScale = Time.timeScale;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            statePaused();
+            activeMenu = pauseMenu;
+            activeMenu.SetActive(isPaused);
+            confineCursor();
+        }
+    }
+
+    public void statePaused()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+    }
+
+    public void stateUnpaused()
+    {
+        isPaused = false;
+        Time.timeScale = timeScale;
+        activeMenu.SetActive(false);
+        activeMenu = null;
+    }
+
+    public void lockCursor()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void confineCursor()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+}
